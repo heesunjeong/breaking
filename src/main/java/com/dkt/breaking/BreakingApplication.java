94b83f1fb7cf9d07adcbc6d5f8af8ecf21589c97
Joy.Jeong <joy.jeong@kakaocorp.com>
2018-07-18 11:02:06 +0900

    init

---
+++ src/main/java/com/dkt/breaking/BreakingApplication.java
@@ -0,0 +1,12 @@
+package com.dkt.breaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakingApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dkt/breaking/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.dkt.breaking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(value = "/")
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
