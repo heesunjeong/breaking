a941f1f410b1d923b27cf73f85d75d671e09dba2
Joy.Jeong <joy.jeong@kakaocorp.com>
2018-11-09 11:52:57 +0900

    리뷰 수정 삭제 기능 추가

--- src/main/java/com/dkt/breaking/controller/ReviewController.java
+++ src/main/java/com/dkt/breaking/controller/ReviewController.java
@@ -4,9 +4,11 @@ import com.dkt.breaking.model.Review;
 import com.dkt.breaking.service.ReviewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +24,6 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
-
     @PostMapping(value = "create")
     /*@PreAuthorize("hasRole('ROLE_USER')")*/
     public Mono<Boolean> insertReview(@RequestBody Review review, ServerWebExchange exchange) {
@@ -32,5 +33,15 @@ public class ReviewController {
     @GetMapping(value = "/{storeId}")
     public Flux<Review> roadReviews(@PathVariable String storeId) {
         return reviewService.readReviews(Mono.just(storeId));
+    }
+
+    @PutMapping(value = "/update")
+    public  Mono<Boolean> updateReview(@RequestBody Review review, ServerWebExchange exchange) {
+        return reviewService.updateReview(review, exchange);
+    }
+
+    @DeleteMapping(value = "/{reviewId}")
+    public Mono<Boolean> deleteReview(@PathVariable String reviewId, ServerWebExchange exchange) {
+        return reviewService.deleteReview(reviewId, exchange);
     }
 }
--- src/main/java/com/dkt/breaking/service/ReviewService.java
+++ src/main/java/com/dkt/breaking/service/ReviewService.java
@@ -22,22 +22,51 @@ public class ReviewService {
     private ReviewRepository reviewRepository;
 
     public Mono<Boolean> createReview(Review review, ServerWebExchange exchange) {
+        String userId = validateTokenAndGetUserId(exchange);
 
-        String userId = userService.getUserIdByToken(userService.getJwtToken(exchange));
-
-        if (userId == "" || userId.isEmpty()) {
-            return Mono.error(new UnsupportedJwtException("Not Supported Token Error"));
-        } else {
-            review.setAuthor(new User(userId));
-            return reviewRepository.save(review)
-                .map(u -> true)
-                .defaultIfEmpty(false);
-        }
+        review.setAuthor(new User(userId));
+        return reviewRepository.save(review)
+            .map(u -> true)
+            .defaultIfEmpty(false);
     }
 
     public Flux<Review> readReviews(Mono<String> storeId) {
         return reviewRepository.findByStoreId(storeId)
-            .filter(review -> !review.getDeleted())
-            .log();
+            .filter(review -> !review.getDeleted());
+    }
+
+    public Mono<Boolean> updateReview(Review review, ServerWebExchange exchange) {
+        String userId = validateTokenAndGetUserId(exchange);
+
+        return reviewRepository.findById(review.getId())
+            .filter(r -> r.getAuthor().getId().equals(userId))
+            .flatMap(r -> {
+                r.setContents(review.getContents());
+                return reviewRepository.save(r);
+            })
+            .map(r -> true)
+            .defaultIfEmpty(false);
+    }
+
+    private String validateTokenAndGetUserId(ServerWebExchange exchange) {
+        String userId = userService.getUserIdByToken(userService.getJwtToken(exchange));
+
+        if (userId == "" || userId.isEmpty()) {
+            new UnsupportedJwtException("Not Supported Token Error");
+        }
+        return userId;
+    }
+
+    public Mono<Boolean> deleteReview(String reviewId, ServerWebExchange exchange) {
+        String userId = validateTokenAndGetUserId(exchange);
+
+        return reviewRepository.findById(reviewId)
+            .filter(r -> r.getAuthor().getId().equals(userId))
+            .flatMap(r -> {
+                r.setDeleted(true);
+                return reviewRepository.save(r);
+            })
+            .map(r -> true)
+            .defaultIfEmpty(false);
     }
 }
